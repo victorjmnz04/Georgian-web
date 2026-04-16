d0be6147deaca73fc2248512cee3d4795c9898b9
victorjmnz04 <victorjmnz04@gmail.com>
2026-04-17 01:17:27 +0200

    -

--- src/components/sections/home-hero.tsx
+++ src/components/sections/home-hero.tsx
@@ -36,7 +36,7 @@ export function HomeHero({ locale }: HomeHeroProps) {
                 <div className="relative aspect-[4/5]">
                   <Image
                     alt="GEORGIAN Chapter 01"
-                    className="object-contain p-6"
+                    className="object-cover object-top"
                     fill
                     priority
                     sizes="(min-width: 1280px) 54vw, (min-width: 768px) 90vw, 100vw"
